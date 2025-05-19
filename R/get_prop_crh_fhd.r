64dd3420527b09f74ebbd4c7bd0be99180e9dd35
Alasdair Robertson <140812830+AlasdairNE@users.noreply.github.com>
2025-05-19 17:40:39 +0100

    Update get_prop_crh_fhd.r
    Altered so yinc is a parameter that can be changed

--- R/get_prop_crh_fhd.r
+++ R/get_prop_crh_fhd.r
@@ -8,6 +8,10 @@
 #' @param d_y Numeric vector with the proportion of birds at height bands across the rotor
 #'   disc
 #'
+#' @param yinc A numeric value, the increment for height bands between the
+#'   lowest and highest points of the rotor, expressed as a proportion of rotor
+#'   radius (defaults to 0.05).
+#'
 #' @return The total proportion of birds at collision risk height derived from a flight
 #'   height distribution
 #'
@@ -29,13 +33,13 @@
 #'  prop_chr_fhd <- get_prop_crh_fhd(d_y)
 #'
 #' @export
-get_prop_crh_fhd <- function(d_y) {
+get_prop_crh_fhd <- function(d_y, yinc = 0.05) {
 
   # number of height bands
   n_bands <- length(d_y)
 
   # integration over the rotor's height bands
-  0.05 * (d_y[1]/2 + d_y[n_bands]/2 + sum(d_y[c(2:(n_bands-1))]))
+  yinc * (d_y[1]/2 + d_y[n_bands]/2 + sum(d_y[c(2:(n_bands-1))]))
 }
 
 
